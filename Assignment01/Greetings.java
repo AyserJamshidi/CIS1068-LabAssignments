77ab40fcddaad45887d1795ddcdb223a3cffecfe
Ayser Jamshidi <acejamshidi4th@gmail.com>
2020-09-12 00:17:32 -0400

    Initial commit

---
+++ Assignment01/Greetings.java
@@ -0,0 +1,16 @@
+/*
+Name: Ayser Jamshidi
+Class: CIS 1068
+Section: 4
+Date: September 4th, 2020
+Assignment: 1. Greetings
+
+Description: 
+This program simply prints a message that includes my lab instructor's name: Branimir Ljubic
+*/
+
+public class Greetings {
+    public static void main(String[] args) {
+        System.out.println("Hello Branimir Ljubic");
+    }
+}
