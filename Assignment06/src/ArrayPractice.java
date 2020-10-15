ffb836376f2e7b45480e1c99f3783d90e03137f3
Ayser Jamshidi <acejamshidi4th@gmail.com>
2020-10-14 21:37:15 -0400

    -Removed all test cases except for a new extra-credit test case. -Removed one of the loops in copyOdds to instead utilize the copyN function to resize the returned array instead of doing two loops where one finds the amount of odd numbers to set a correct array length for the returning array.
    -Added null and length checks to most functions with proper returns.
    -Added function isSortedAscending(Integer[]) Returns true if an array is sorted in ascending order, otherwise false.
    -Added function sortedAscendingRun(Integer[], Integer): Returns the amount of sorted ascending numbers (as an integer) starting from a specified index (x) and continuing until an element is less than the current index.
    -Added function copyAll(Integer[], Integer[]): Returns a new array that combines two arrays in the order given.
    -Added function reverse(Integer[]): Reverses the position of all elements in an array.
    -Added extra credit function uniques(Integer[]): Returns a new array with only unique elements from the referenced array.
    
    -Fixed a bug where findN would go out of bounds if the maximum index (n) was greater than the array's length.
    -Fixed a bug where copyN would return an empty array instead of null when the given amount of elements to copy (n) was less than or equal to 0.

--- Assignment06/src/ArrayPractice.java
+++ Assignment06/src/ArrayPractice.java
@@ -1,127 +1,20 @@
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.stream.IntStream;
-
 /**
  * @author Ayser Jamshidi
  */
 public class ArrayPractice {
 
 	public static void main(String[] args) {
-		int[] testy = new int[10];
-		initialize(testy, 3);
+		int[] A = uniques(new int[]{-1, 0, 0, 1, 1, 2, 2, 3, 3, 4, 4, 4, 4, 5, 4, -1, 9, 11, 10});
 
-		for (int i : testy) {
-			System.out.print(i + ", ");
-		}
-
-		// Test 2-3
-		System.out.println("\nAverage: " + average(new int[]{1, 10})); // Expect 5.5
-		System.out.println("Average: " + average(new int[]{0, 10})); // Expect 5
-
-		// Test 3
-		System.out.println("numOccurrences: " + numOccurrences(new int[]{0, 0, 0, 3}, 0)); // Expect 3
-		System.out.println("numOccurrences: " + numOccurrences(new int[]{0, 2, 2, 6, 34, 2, 5}, 5)); // Expect 1
-
-		// Test 4
-		System.out.println("find: " + find(new int[]{0, 1, 7, 2, 3, 3, 4}, 7)); // Expect 2
-		System.out.println("find: " + find(new int[]{0, 1, 7, 2, 3, 3, 4}, 3)); // Expect 4
-
-		System.out.println("findN: " + findN(new int[]{0, 2, 3, 3, 3, 3, 6}, 6, 6)); // Expect -1
-		System.out.println("findN: " + findN(new int[]{0, 2, 3, 3, 3, 3, 6}, 6, 7)); // Expect 6
-		System.out.println("findN: " + findN(new int[]{0, 2, 3, 3, 3, 3, 6}, 7, 30)); // Expect -1
-
-		System.out.println("findLast: " + findLast(new int[]{3, 1, 2, 5, 7, 6}, 6)); // Expect 5
-		System.out.println("findLast: " + findLast(new int[]{3, 1, 2, 5, 7, 6}, 4)); // Expect -1
-
-		System.out.println("largest: " + largest(new int[]{3, 3, 5, 7, 349, 1, 3, 6, 2, 2, 2, 361, 22}));
-
-		System.out.println("indexOfLargest: " + indexOfLargest(new int[]{0, 1, 2, 33, 4, 56, 7, 8})); // Expect 5
-		System.out.println("indexOfLargest: " + indexOfLargest(new int[]{0, -1, -2})); // Expect 0
-
-		System.out.println("indexOfLargestOdd: " + indexOfLargestOdd(new int[]{0, 2, 4, 6, 8, 10})); // Expect -1
-		System.out.println("indexOfLargestOdd: " + indexOfLargestOdd(new int[]{0, 2, 3, 8, 9, 10})); // Expect 4
-		System.out.println("indexOfLargestOdd: " + indexOfLargestOdd(new int[]{9, -7, -3, 0, 8, 11, 10})); // Expect 5
-
-		{
-			System.out.print("copy: ");
-			int[] A = new int[]{1, 3, 7, 7, 3};
-			int[] B = copy(A);
-
-			for (int i = 0; i < B.length; i++) {
-				if (B[i] == A[i])
-					System.out.print(B[i] + " ");
-				else
-					System.out.println("B[" + i + "] IS NOT GUCCI");
-			}
-			System.out.println("       Done.  Array references are the same == " + (A == B));
-		}
-
-
-		{
-			System.out.print("copyN: ");
-			int expectedLength = 30;
-			int[] A = copyN(new int[]{1, 3, 4, 5, 7, 8, 9, 9, 9, 9}, expectedLength);
-
-			for (int i : A)
-				System.out.print(i + " ");
-		}
-
-		System.out.println();
-
-		{
-			System.out.print("copyOdds: ");
-			int[] A = copyOdds(new int[]{-2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10});
-
-			for (int i : A)
-				if (i % 2 == 0)
-					System.out.print("BAD NUMBER " + i + " WAS ADDED");
-				else
-					System.out.print(i + " ");
-		}
-
-		System.out.println();
-
-		{
-			int[] A = new int[]{0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
-			for (int i : A)
-				System.out.print(i + " ");
-			System.out.println();
-			int removed = remove(A, 3);
-
-			for (int i : A)
-				System.out.print(i + " ");
-
-			System.out.println("        Removed number " + removed);
-		}
-
-		System.out.println();
-
-		{
-			int[] A = new int[]{0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
-			for (int i : A)
-				System.out.print(i + " ");
-			System.out.println();
-
-			shiftLeft(A);
-
-			for (int i : A)
-				System.out.print(i + " ");
-		}
-
-		System.out.println();
+		for (int i : A)
+			System.out.println(i + " ");
 	}
 
 	/* sets every item in the array A references to initialValue */
 	public static void initialize(int[] A, int initialValue) {
-		/*
-		 * This loop sets every element in A beginning from index 0 to initialValue.
-		 *
-		 * This code does exactly what Arrays.fill does except without setting a variable to the array length
-		 * for no reason.
-		 */
-		for (int curIndex = 0; curIndex < A.length; curIndex++)
-			A[curIndex] = initialValue;
+		if (A != null && A.length > 0)
+			for (int i = 0; i < A.length; i++)
+				A[i] = initialValue;
 	}
 
 	/*
@@ -129,11 +22,11 @@ public class ArrayPractice {
 	 * but the method returns double. What do we do to handle this?
 	 */
 	public static double average(int[] A) {
+		if (A == null || A.length <= 0)
+			return 0;
+
 		double total = 0; // Set the default output to 0 because if nothing is in the array, then return 0
 
-		/*
-		 *
-		 */
 		for (int curInt : A)
 			total += curInt;
 
@@ -142,6 +35,9 @@ public class ArrayPractice {
 
 	/* returns the number of times that x appears in the array A references */
 	public static int numOccurrences(int[] A, int x) {
+		if (A == null || A.length <= 0)
+			return 0;
+
 		int total = 0;
 
 		for (int curInt : A)
@@ -156,6 +52,9 @@ public class ArrayPractice {
 	 * exist in the array
 	 */
 	public static int find(int[] A, int x) {
+		if (A == null || A.length <= 0)
+			return -1;
+
 		for (int curIndex = 0; curIndex < A.length; curIndex++)
 			if (A[curIndex] == x)
 				return curIndex;
@@ -167,10 +66,17 @@ public class ArrayPractice {
 	 * references or -1 if item is not among the first n elements of the array
 	 */
 	public static int findN(int[] A, int item, int n) {
+		if (A == null || A.length <= 0)
+			return -1;
 
-		for (int curIndex = 0; (curIndex < n && curIndex < A.length); curIndex++)
+		// Ensures that if the user makes n greater than A.length, we do this to avoid going out of bounds.
+		if (n > A.length)
+			n = A.length;
+
+		for (int curIndex = 0; curIndex < n; curIndex++)
 			if (A[curIndex] == item)
 				return curIndex;
+
 		return -1;
 	}
 
@@ -179,6 +85,9 @@ public class ArrayPractice {
 	 * exist in the array
 	 */
 	public static int findLast(int[] A, int x) {
+		if (A == null || A.length <= 0)
+			return -1;
+
 		for (int curIndex = (A.length - 1); curIndex >= 0; curIndex--)
 			if (A[curIndex] == x)
 				return curIndex;
@@ -187,6 +96,9 @@ public class ArrayPractice {
 
 	/* returns the largest item found in the array A references */
 	public static int largest(int[] A) {
+		if (A == null || A.length <= 0)
+			return 0; // We return 0 because that's what integers initialize to.
+
 		int largestInt = Integer.MIN_VALUE;
 
 		for (int curInt : A)
@@ -198,6 +110,9 @@ public class ArrayPractice {
 
 	/* returns the index of the largest item found in the array A references */
 	public static int indexOfLargest(int[] A) {
+		if (A == null || A.length <= 0)
+			return -1; // We return -1 to indicate no index was found.
+
 		int largestInt = Integer.MIN_VALUE, largestIntIndex = -1;
 
 		for (int curIndex = 0; curIndex < A.length; curIndex++)
@@ -214,6 +129,9 @@ public class ArrayPractice {
 	 * contains no odd numbers
 	 */
 	public static int indexOfLargestOdd(int[] A) {
+		if (A == null || A.length <= 0)
+			return -1;
+
 		int largestOddInt = Integer.MIN_VALUE, largestOddIntIndex = -1;
 
 		for (int curIndex = 0; curIndex < A.length; curIndex++) {
@@ -231,6 +149,9 @@ public class ArrayPractice {
 	 * returns a new array consisting of all of the elements of A[]
 	 */
 	public static int[] copy(int[] A) {
+		if (A == null || A.length <= 0)
+			return new int[0];
+
 		return A.clone();
 	}
 
@@ -240,7 +161,10 @@ public class ArrayPractice {
 	 * exactly the same as A, and the remaining n-A.length elements set to 0. If n<=0, returns null.
 	 */
 	public static int[] copyN(int[] A, int n) {
-		int[] B = (n >= 0) ? new int[n] : null;
+		if (A == null || A.length <= 0)
+			return new int[0];
+
+		int[] B = (n > 0) ? new int[n] : null;
 
 		if (B != null)
 			for (int i = 0; i < n; i++)
@@ -256,24 +180,20 @@ public class ArrayPractice {
 	 * returns a reference to an array consisting of all of the elements of the array A references
 	 * that are odd. If there are no odd integers in the array, the function returns null.
 	 */
-	public static int[] copyOdds(int[] A) { /// TODO: This could totally look less ugly
-		int lengthOfNewArray = 0;
+	public static int[] copyOdds(int[] A) {
+		if (A == null || A.length <= 0)
+			return new int[0];
 
-		for (int j : A)
-			if (j % 2 != 0)
-				lengthOfNewArray++;
+		int[] B = new int[A.length];
+		int bCurIndex = 0;
 
-		int[] B = new int[lengthOfNewArray];
-
-		int currentLength = 0;
-
-		for (int j : A)
-			if (j % 2 != 0) {
-				B[currentLength] = j;
-				currentLength++;
+		for (int i : A)
+			if (i % 2 != 0) {
+				B[bCurIndex] = i;
+				bCurIndex++;
 			}
 
-		return B;
+		return copyN(B, bCurIndex);
 	}
 
 	/* removes and returns the item at index x shifting all elements at */
@@ -297,23 +217,23 @@ public class ArrayPractice {
 
 	/* and the function returns 30 */
 	public static int remove(int[] A, int x) {
+		if (A == null || x >= A.length || x < 0 || A.length <= 0)
+			return -1;
+
 		int removedNum = -1;
+		int curShiftAmount = 0;
 
-		if (x >= 0 && !(A.length <= x)) {
-			int curShiftAmount = 0;
-
-			// Skip index x and move everything to the left once.
-			for (int i = 0; i < A.length; i++) {
-				if (i == x) {
-					curShiftAmount++;
-					removedNum = A[i];
-				} else
-					A[i - curShiftAmount] = A[i];
-			}
-
-			// Sets the last index to 0 because everything shifted to the left.
-			A[A.length - 1] = 0;
+		// Skip index x and move everything to the left once.
+		for (int i = 0; i < A.length; i++) {
+			if (i == x) {
+				curShiftAmount++;
+				removedNum = A[i];
+			} else
+				A[i - curShiftAmount] = A[i];
 		}
+
+		// Sets the last index to 0 because everything shifted to the left.
+		A[A.length - 1] = 0;
 
 		return removedNum;
 	}
@@ -342,8 +262,11 @@ public class ArrayPractice {
 	 * returns true if A is in sorted ascending order and false otherwise
 	 */
 	public static boolean isSortedAscending(int[] A) {
+		for (int i = 0; i < (A.length - 1); i++)
+			if (A[i] > A[i + 1])
+				return false;
 
-		return false;
+		return true;
 	}
 
 	/* Returns the number of items in the array that A references starting at index x that are in */
@@ -365,7 +288,16 @@ public class ArrayPractice {
 	 *
 	 */
 	public static int sortedAscendingRun(int[] A, int x) {
-		return -1;
+		int actualAscendingCount = 1; // We always start at 1 because we count the initial index
+
+		for (int i = x; i < (A.length - 1); i++) {
+			if (A[i] > A[i + 1])
+				break;
+
+			actualAscendingCount++;
+		}
+
+		return actualAscendingCount;
 	}
 
 	/*
@@ -374,7 +306,16 @@ public class ArrayPractice {
 	 * {10,20,30,5,9,38}
 	 */
 	public static int[] copyAll(int[] A, int[] B) {
-		return null;
+		int[] C = new int[A.length + B.length];
+		int curIndex = 0;
+
+		for (int[] curArray : new int[][]{A, B})
+			for (int curInt : curArray) {
+				C[curIndex] = curInt;
+				curIndex++;
+			}
+
+		return C;
 	}
 
 	/*
@@ -382,6 +323,13 @@ public class ArrayPractice {
 	 * method, A[] would be {50,40,30,20,10}
 	 */
 	public static void reverse(int[] A) {
+		if (A == null || A.length <= 0)
+			return;
+
+		int[] containerArr = A.clone();
+
+		for (int i = 0; i < A.length; i++)
+			A[i] = containerArr[(A.length - 1) - i];
 	}
 
 	/*
@@ -391,6 +339,37 @@ public class ArrayPractice {
 	 * example, if A[] is {10,20,5,32,5,10,9,32,8}, the method returns the array {10,20,5,32,9,8}
 	 */
 	public static int[] uniques(int[] A) {
-		return null;
+		if (A == null || A.length <= 0)
+			return new int[0];
+
+		int[] B = new int[A.length];
+		int newArrIndex = 0;
+
+		for (int curIndex = 0; curIndex < A.length; curIndex++) {
+			if (isPreviouslyUnique(A, curIndex)) {
+				B[newArrIndex] = A[curIndex];
+				newArrIndex++;
+			}
+		}
+
+		return copyN(B, newArrIndex);
+	}
+
+	/**
+	 * Loops through {@code A} until the index {@code uniqueIndex} (exclusive) and checks if any previous element
+	 * is equal to A[uniqueIndex].
+	 * <p>
+	 * This function's main use it to ensure that the element at uniqueIndex is not equal to any element in indexes
+	 * before it, to ensure uniqueness.
+	 *
+	 * @param A           the Array to test for previous uniqueness
+	 * @param uniqueIndex The index of the element you would like to check is unique
+	 * @return <b>true</b> if A[uniqueIndex] is not equal to any previous element, <b>false</b> otherwise.
+	 */
+	private static boolean isPreviouslyUnique(int[] A, int uniqueIndex) {
+		for (int curSampleIndex = 0; curSampleIndex < uniqueIndex; curSampleIndex++)
+			if (A[curSampleIndex] == A[uniqueIndex])
+				return false;
+		return true;
 	}
 }
