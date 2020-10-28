3c8d7d2a5aca55bc4232e4d3b5453a96fe1ad335
Ayser Jamshidi <acejamshidi4th@gmail.com>
2020-10-28 18:25:06 -0400

    Little progress again

--- Assignment07/src/BookRecommender.java
+++ Assignment07/src/BookRecommender.java
@@ -29,165 +29,206 @@
 
 import java.io.BufferedReader;
 import java.io.FileReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Scanner;
+import java.lang.reflect.Array;
+import java.util.*;
 
 @SuppressWarnings("all")
 public class BookRecommender {
 
-    private final static String BOOKS_LIST_PATH = "Assignment07/src/BookList.txt";
-    private final static String RATINGS_LIST_PATH = "Assignment07/src/BookRatings.txt";
+	private final int NUM_RECOMMENDATIONS = 3;
 
-    public static void main(String[] args) {
-        List<String> bookListContainer = fileToList(BOOKS_LIST_PATH);
-        List<String> ratingListContainer = fileToList(RATINGS_LIST_PATH);
+	private final static String BOOKS_LIST_PATH = "Assignment07/src/BookList.txt";
+	private final static String RATINGS_LIST_PATH = "Assignment07/src/BookRatings.txt";
 
-        // [user][user's ratings]
-        int[][] databaseRatings = new int[ratingListContainer.size()][ratingListContainer.get(0).split(" ").length];
+	public static void main(String[] args) {
+		List<String> bookListContainer = fileToList(BOOKS_LIST_PATH);
+		List<String> ratingListContainer = fileToList(RATINGS_LIST_PATH);
 
-        for (int i = 0; i < ratingListContainer.size(); i++) {
-            String[] StringToIntArray = ratingListContainer.get(i).split(" ");
+		// [user][book index's rating]
+		int[][] databaseRatings = new int[ratingListContainer.size()][ratingListContainer.get(0).split(" ").length];
 
-            for (int j = 0; j < StringToIntArray.length; j++)
-                databaseRatings[i][j] = Integer.parseInt(StringToIntArray[j]);
-        }
+		for (int i = 0; i < ratingListContainer.size(); i++) {
+			String[] StringToIntArray = ratingListContainer.get(i).split(" ");
 
-        // array[bookRow][currentUserRatingColumn]
-		//int[][] ratingList = assignRatingsToBooks(bookListContainer, ratingListContainer);//new int[bookListContainer.size()][ratingListContainer.size()];
+			for (int j = 0; j < StringToIntArray.length; j++)
+				databaseRatings[i][j] = Integer.parseInt(StringToIntArray[j]);
+		}
 
-        //int[] humanUserRatings = userInput(bookListContainer);
-        int[] humanUserRatings = {1, 1, 2, 3, 5, 3, 1, -1, -1, -1, 4, 4, 5, 1, 2, 3, 1, 2, -1, -1};
+		//int[] humanUserRatings = userInput(bookListContainer);
+		int[] humanUserRatings = {-1, 1, 1, 1, 1, 1, 1, 1, 1, 1, -1, 1, 1, 1, 1, -1, 1, 1, 1, -1}; // Our input, it's a copy of the last user (30) but subtracting 1 star from the final book
 
-        double[] similarityRatingsArr = new double[databaseRatings.length];
-        int mostSimilarDbUserId = -1;
+		double[] cosSimilarityRatings = new double[databaseRatings.length]; // 30 user similarity ratings when taking into account every book
 
-        for (int i = 0; i < similarityRatingsArr.length; i++) {
-	        similarityRatingsArr[i] = cosineSimilarity(humanUserRatings, databaseRatings[i]);
+		double highestSimilarityRating = -1;
+		int mostSimilarDbUserId = -1;
 
-	        if (similarityRatingsArr[i] > mostSimilarDbUserId)
-	        	mostSimilarDbUserId = i;
-        }
+		for (int i = 0; i < cosSimilarityRatings.length; i++) {
+			cosSimilarityRatings[i] = cosineSimilarity(humanUserRatings, databaseRatings[i]);
 
-        System.out.println(Arrays.toString(similarityRatingsArr));
-    }
-
-    public static int[] stringToIntArray(String stringList) {
-        String[] stringArr = stringList.split(" ");
-        int[] intArr = new int[stringArr.length];
-
-        for (int i = 0; i < intArr.length; i++)
-            intArr[i] = Integer.parseInt(stringArr[i]);
-
-        return intArr;
-    }
-
-    /**
-     * Gets a cos similarity value comparing each read book between {@code humanUserRatings} and {@code databaseRatings}
-     * Both arrays must have the same array length or it is impossible to get a cos similarity between them.
-     *
-     * @param humanUserRatings current human user's array of ratings
-     * @param databaseRatings current database index's array of ratings.
-     * @return A double at the intervals [0, 1] or -1 if the array lengths aren't equal
-     */
-    public static double cosineSimilarity(int[] humanUserRatings, int[] databaseRatings) {
-        // Ensure if both arrays exist and comparable
-        if (humanUserRatings == null || databaseRatings == null || humanUserRatings.length != databaseRatings.length)
-            return -1;
-
-        // Variable init for human and database
-        double humanSum = 0, databaseSum = 0;
-        List<Integer> similarBooksIndexes = new ArrayList<>();
-
-        for (int i = 0; i < humanUserRatings.length; i++) { // Loop through every rating
-            if (humanUserRatings[i] != -1) // Square current rating for human's ratings
-                humanSum += Math.pow(humanUserRatings[i], 2);
-
-            if (databaseRatings[i] != -1) // Square current rating for current database user's ratings
-                databaseSum += Math.pow(databaseRatings[i], 2);
-
-            if (humanUserRatings[i] != -1 && databaseRatings[i] != -1) // The human and the current user read this book
-                similarBooksIndexes.add(i);
-        }
-
-        humanSum = Math.sqrt(humanSum); // Square both to get original value as abs
-        databaseSum = Math.sqrt(databaseSum);
-
-        double both = 0;
-
-        for (int bookIndex : similarBooksIndexes)
-            both += (humanUserRatings[bookIndex] * databaseRatings[bookIndex]);
-
-        return both / (humanSum * databaseSum);
-    }
-
-    public static int[][] assignRatingsToBooks(List<String> books, List<String> ratings) {
-        int[][] booksAndRatings = new int[books.size()][ratings.size()];
-
-        for (int book = 0, user = 0; user < ratings.size(); user++, book = 0) // Loop the amount of lines of RATINGS_LIST_PATH file
-            for (String curStrInteger : ratings.get(user).split(" ")) { // Array of Integers as Strings
-                booksAndRatings[book][user] = Integer.parseInt(curStrInteger);
-                book++;
-            }
-
-        return booksAndRatings;
-    }
-
-    public static int[] userInput(List<String> bookList) {
-        int[] currentUserRatings = new int[bookList.size()];
-        Scanner in = new Scanner(System.in);
-
-        for (int i = 0; i < bookList.size(); i++) {
-            boolean hasInputIncorrectly = false;
-
-            while ((currentUserRatings[i] != -1 && currentUserRatings[i] < 1) || 5 < currentUserRatings[i]) {
-                if (hasInputIncorrectly)
-                    System.out.println(currentUserRatings[i] + " is not a proper rating.");
-
-                System.out.print("Input a rating from 1-5, or -1 if you haven't read \"" + bookList.get(i) + "\": ");
-
-                currentUserRatings[i] = in.nextInt();
-                hasInputIncorrectly = true;
-                System.out.println();
-            }
-        }
-
-        return currentUserRatings;
-    }
-
-    public static int countFileLines(String filePath) {
-        return fileToList(filePath).size();
-    }
-
-    public static List<String> fileToList(String filePath) {
-        List<String> returnedList = new ArrayList<>();
-
-        try {
-            BufferedReader in = new BufferedReader(new FileReader(filePath));
-
-            for (String curLine = in.readLine(); curLine != null; curLine = in.readLine())
-                returnedList.add(curLine);
-
-        } catch (Exception e) {
-            System.out.println("The file " + filePath + " couldn't be read!");
-        }
-
-        return returnedList;
-    }
-
-	/*public static double weightedAverage(double[] A, int[][] booksAndRatings, int userNum) {
-		double sumScoreWeightProduct = 0, sumWeights = 0;
-
-		for (int bookNum = 0; bookNum < booksAndRatings.length; bookNum++) {
-			if (booksAndRatings[bookNum][userNum] != -1) {
-				sumScoreWeightProduct += (booksAndRatings[bookNum][userNum] * A[bookNum]);
-				sumWeights += A[bookNum];
+			if (cosSimilarityRatings[i] > highestSimilarityRating) {
+				highestSimilarityRating = cosSimilarityRatings[i];
+				mostSimilarDbUserId = i;
 			}
 		}
 
-		return sumScoreWeightProduct / sumWeights;
-	}*/
+		double[] bookRatingsWeighted = averageThing(cosSimilarityRatings, databaseRatings);
+
+
+		final int NUM_RECOMMENDATIONS = 3;
+
+		double highestRating = -1;
+		int highestRatedBookIndex = -1;
+
+		TreeMap<Double, Integer> testy = new TreeMap<>();
+
+		// 0.99, 13
+		System.out.println();
+
+		for (int i = 0; i < bookRatingsWeighted.length; i++) {
+			if (humanUserRatings[i] == -1) { // If we haven't read the book
+
+//				if (bookRatingsWeighted[i] > highestRating) {
+//					System.out.println("Inside! 2");ªªª
+//					highestRating = bookRatingsWeighted[i];
+//					highestRatedBookIndex = i;
+//				}
+			}
+		}
+
+		System.out.println("You should read \"" + bookListContainer.get(highestRatedBookIndex) + "\"!");
+
+	}
+
+	public static int[] stringToIntArray(String stringList) {
+		if (stringList.length() <= 0)
+			return null;
+
+		String[] stringArr = stringList.split(" ");
+		int[] intArr = new int[stringArr.length];
+
+		for (int i = 0; i < intArr.length; i++)
+			intArr[i] = Integer.parseInt(stringArr[i]);
+
+		return intArr;
+	}
+
+	/**
+	 * Gets a cos similarity value comparing each read book between {@code humanUserRatings} and {@code databaseRatings}
+	 * Both arrays must have the same array length or it is impossible to get a cos similarity between them.
+	 *
+	 * @param humanUserRatings current human user's array of ratings
+	 * @param databaseRatings  current database index's array of ratings.
+	 * @return A double at the intervals [0, 1] or -1 if the array lengths aren't equal
+	 */
+	public static double cosineSimilarity(int[] humanUserRatings, int[] databaseRatings) {
+		// Ensure if both arrays exist and comparable
+		if (humanUserRatings == null || databaseRatings == null || humanUserRatings.length != databaseRatings.length)
+			return -1;
+
+		// Variable init for human and database
+		double humanSum = 0, databaseSum = 0;
+		List<Integer> similarBooksIndexes = new ArrayList<>();
+
+		for (int i = 0; i < humanUserRatings.length; i++) { // Loop through every rating
+			if (humanUserRatings[i] != -1) // Square current rating for human's ratings
+				humanSum += Math.pow(humanUserRatings[i], 2);
+
+			if (databaseRatings[i] != -1) // Square current rating for current database user's ratings
+				databaseSum += Math.pow(databaseRatings[i], 2);
+
+			if (humanUserRatings[i] != -1 && databaseRatings[i] != -1) // The human and the current user read this book
+				similarBooksIndexes.add(i);
+		}
+
+		humanSum = Math.sqrt(humanSum); // Square both to get original value as abs
+		databaseSum = Math.sqrt(databaseSum);
+
+		double both = 0;
+
+		for (int bookIndex : similarBooksIndexes)
+			both += (humanUserRatings[bookIndex] * databaseRatings[bookIndex]);
+
+		return both / (humanSum * databaseSum);
+	}
+
+	public static int[][] assignRatingsToBooks(List<String> books, List<String> ratings) {
+		int[][] booksAndRatings = new int[books.size()][ratings.size()];
+
+		for (int book = 0, user = 0; user < ratings.size(); user++, book = 0) // Loop the amount of lines of RATINGS_LIST_PATH file
+			for (String curStrInteger : ratings.get(user).split(" ")) { // Array of Integers as Strings
+				booksAndRatings[book][user] = Integer.parseInt(curStrInteger);
+				book++;
+			}
+
+		return booksAndRatings;
+	}
+
+	public static int[] userInput(List<String> bookList) {
+		int[] currentUserRatings = new int[bookList.size()];
+		Scanner in = new Scanner(System.in);
+
+		for (int i = 0; i < bookList.size(); i++) {
+			boolean hasInputIncorrectly = false;
+
+			while ((currentUserRatings[i] != -1 && currentUserRatings[i] < 1) || 5 < currentUserRatings[i]) {
+				if (hasInputIncorrectly)
+					System.out.println(currentUserRatings[i] + " is not a proper rating.");
+
+				System.out.print("Input a rating from 1-5, or -1 if you haven't read \"" + bookList.get(i) + "\": ");
+
+				currentUserRatings[i] = in.nextInt();
+				hasInputIncorrectly = true;
+				System.out.println();
+			}
+		}
+
+		return currentUserRatings;
+	}
+
+	public static int countFileLines(String filePath) {
+		return fileToList(filePath).size();
+	}
+
+	public static List<String> fileToList(String filePath) {
+		List<String> returnedList = new ArrayList<>();
+
+		try {
+			BufferedReader in = new BufferedReader(new FileReader(filePath));
+
+			for (String curLine = in.readLine(); curLine != null; curLine = in.readLine())
+				returnedList.add(curLine);
+
+		} catch (Exception e) {
+			System.out.println("The file " + filePath + " couldn't be read!");
+		}
+
+		return returnedList;
+	}
+
+	/**
+	 * @param cosSimilarities
+	 * @param databaseRatings
+	 * @return
+	 */
+	public static double[] averageThing(double[] cosSimilarities, int[][] databaseRatings) {
+		if (cosSimilarities == null || databaseRatings == null || cosSimilarities.length <= 0 || databaseRatings.length <= 0)
+			return null;
+
+		double[] returningArray = new double[databaseRatings[0].length]; // Set it to the amount of books
+
+		// for book 0, get all 30 ratings: (((user 0 rating * user 0 cos similarity) + (user 1 rating * user 1 cos similarity) + ...)) / (user 0 cos similarity + user 1 cos similarity + ...)
+		for (double bookIndex = 0, sumOfWeightedScores = 0, sumOfWeights = 0; bookIndex < databaseRatings[0].length; bookIndex++, sumOfWeightedScores = 0, sumOfWeights = 0) { // Cycle through every book
+			for (int dbUserIndex = 0; dbUserIndex < databaseRatings.length; dbUserIndex++) // Cycle through every db user
+				if (databaseRatings[dbUserIndex][(int) bookIndex] != -1) { // Ensure the current db user has read the book
+					sumOfWeightedScores += (databaseRatings[dbUserIndex][(int) bookIndex] * cosSimilarities[dbUserIndex]); // user's book index rating * user's cos similarity
+					sumOfWeights += cosSimilarities[dbUserIndex]; // add weight to 
+				}
+
+			returningArray[(int) bookIndex] = sumOfWeightedScores / sumOfWeights;
+		}
+
+		return returningArray;
+	}
 }
 
 
@@ -205,34 +246,34 @@ Books:
 A -1 rating means this user hasn't read the book
 
 Ratings:
--1 2 3 5 -1 5 3 3 1 4 2 2 5 -1 1 3 3 5 4 3
--1 1 1 4 1 3 3 1 2 3 4 -1 4 1 2 4 5 4 2 3
-3 -1 2 3 -1 2 5 -1 3 3 5 2 2 1 2 3 5 3 4 2
--1 1 -1 4 1 3 5 2 1 5 3 -1 5 2 1 3 4 5 3 2
--1 -1 3 2 -1 5 5 2 2 4 4 2 3 2 -1 3 4 4 3 1
-2 1 1 5 2 2 4 2 3 4 3 -1 5 2 2 5 3 5 2 1
-3 -1 3 4 -1 2 5 -1 -1 4 3 -1 3 -1 2 5 5 5 4 2
-4 -1 4 2 3 -1 1 3 4 -1 1 4 4 4 -1 2 -1 1 4 4
-4 3 3 3 -1 2 2 4 3 -1 2 4 3 4 2 -1 -1 2 2 3
-3 -1 3 -1 3 4 -1 5 5 -1 -1 -1 1 -1 -1 1 1 2 -1 5
-3 -1 3 4 3 4 -1 5 5 2 3 3 4 1 1 -1 -1 -1 -1 4
-4 -1 4 4 1 3 -1 5 4 -1 1 3 4 1 -1 1 -1 1 -1 5
-5 -1 3 1 4 3 -1 5 4 1 3 2 1 -1 4 2 1 -1 2 4
-3 -1 5 1 4 4 2 5 5 1 2 3 1 1 -1 1 -1 1 -1 5
-4 1 5 4 3 -1 1 3 4 -1 -1 3 3 -1 1 1 2 -1 3 5
--1 1 1 3 -1 3 1 3 -1 -1 3 -1 5 2 2 1 4 -1 5 -1
-3 -1 2 3 1 5 4 3 3 -1 5 -1 5 2 -1 4 4 3 3 3
-1 1 1 3 2 4 1 -1 -1 -1 5 -1 3 -1 -1 1 -1 2 5 2
--1 2 3 5 -1 4 3 1 1 3 3 -1 4 -1 -1 4 3 2 5 1
--1 1 3 3 -1 3 3 1 -1 -1 3 -1 5 -1 -1 3 1 2 4 -1
-3 -1 2 4 1 4 3 -1 2 3 4 1 3 -1 2 -1 4 3 5 -1
--1 1 3 5 -1 4 2 1 -1 3 3 2 3 2 -1 3 1 -1 3 -1
-3 2 2 3 -1 5 -1 -1 2 3 4 -1 4 1 -1 -1 -1 -1 4 2
--1 3 -1 -1 4 -1 2 -1 2 2 2 5 -1 3 4 -1 -1 2 -1 2
-1 4 3 -1 3 2 1 -1 -1 -1 1 3 1 3 3 1 -1 -1 -1 3
-4 3 3 -1 4 2 -1 4 -1 -1 2 4 -1 3 4 2 -1 -1 -1 4
--1 5 1 -1 4 1 -1 3 2 2 -1 4 1 3 3 1 -1 -1 -1 3
--1 4 2 1 5 -1 -1 2 1 1 -1 5 -1 5 4 1 2 2 -1 1
-2 5 2 -1 3 -1 -1 1 -1 2 -1 4 2 4 3 -1 2 1 -1 -1
-2 5 1 1 4 -1 2 1 -1 -1 2 4 -1 3 4 2 -1 -1 -1 4
+-1  2  3  5 -1  5  3  3  1  4  2  2  5 -1  1  3  3  5  4  3
+-1  1  1  4  1  3  3  1  2  3  4 -1  4  1  2  4  5  4  2  3
+ 3 -1  2  3 -1  2  5 -1  3  3  5  2  2  1  2  3  5  3  4  2
+-1  1 -1  4  1  3  5  2  1  5  3 -1  5  2  1  3  4  5  3  2
+-1 -1  3  2 -1  5  5  2  2  4  4  2  3  2 -1  3  4  4  3  1
+ 2  1  1  5  2  2  4  2  3  4  3 -1  5  2  2  5  3  5  2  1
+ 3 -1  3  4 -1  2  5 -1 -1  4  3 -1  3 -1  2  5  5  5  4  2
+ 4 -1  4  2  3 -1  1  3  4 -1  1  4  4  4 -1  2 -1  1  4  4
+ 4  3  3  3 -1  2  2  4  3 -1  2  4  3  4  2 -1 -1  2  2  3
+ 3 -1  3 -1  3  4 -1  5  5 -1 -1 -1  1 -1 -1  1  1  2 -1  5
+ 3 -1  3  4  3  4 -1  5  5  2  3  3  4  1  1 -1 -1 -1 -1  4
+ 4 -1  4  4  1  3 -1  5  4 -1  1  3  4  1 -1  1 -1  1 -1  5
+ 5 -1  3  1  4  3 -1  5  4  1  3  2  1 -1  4  2  1 -1  2  4
+ 3 -1  5  1  4  4  2  5  5  1  2  3  1  1 -1  1 -1  1 -1  5
+ 4  1  5  4  3 -1  1  3  4 -1 -1  3  3 -1  1  1  2 -1  3  5
+-1  1  1  3 -1  3  1  3 -1 -1  3 -1  5  2  2  1  4 -1  5 -1
+ 3 -1  2  3  1  5  4  3  3 -1  5 -1  5  2 -1  4  4  3  3  3
+ 1  1  1  3  2  4  1 -1 -1 -1  5 -1  3 -1 -1  1 -1  2  5  2
+-1  2  3  5 -1  4  3  1  1  3  3 -1  4 -1 -1  4  3  2  5  1
+-1  1  3  3 -1  3  3  1 -1 -1  3 -1  5 -1 -1  3  1  2  4 -1
+ 3 -1  2  4  1  4  3 -1  2  3  4  1  3 -1  2 -1  4  3  5 -1
+-1  1  3  5 -1  4  2  1 -1  3  3  2  3  2 -1  3  1 -1  3 -1
+ 3  2  2  3 -1  5 -1 -1  2  3  4 -1  4  1 -1 -1 -1 -1  4  2
+-1  3 -1 -1  4 -1  2 -1  2  2  2  5 -1  3  4 -1 -1  2 -1  2
+ 1  4  3 -1  3  2  1 -1 -1 -1  1  3  1  3  3  1 -1 -1 -1  3
+ 4  3  3 -1  4  2 -1  4 -1 -1  2  4 -1  3  4  2 -1 -1 -1  4
+-1  5  1 -1  4  1 -1  3  2  2 -1  4  1  3  3  1 -1 -1 -1  3
+-1  4  2  1  5 -1 -1  2  1  1 -1  5 -1  5  4  1  2  2 -1  1
+ 2  5  2 -1  3 -1 -1  1 -1  2 -1  4  2  4  3 -1  2  1 -1 -1
+ 2  5  1  1  4 -1  2  1 -1 -1  2  4 -1  3  4  2 -1 -1 -1  4
  */
